eb14f8c98d5083fa7d55df943162e98b41c29a88
Carlosc23 <cal15219@uvg.edu.gt>
2019-09-21 00:39:59 -0600

    feat: add vuerouter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: () => import("./components/CheckList.vue")
+    },
+    {
+      path: "/ControlCheck",
+      component: () => import("./components/ControlCheck.vue")
+    },
+    {
+      path: "/DomainCheck",
+      component: () => import("./components/DomainCheck.vue")
+    },
+    {
+      path: "/Graphics",
+      component: () => import("./components/Graphics.vue")
+    },
+    {
+      path: "/Dictionary",
+      component: () => import("./components/Dictionary.vue")
+    }
+  ]
+});
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
