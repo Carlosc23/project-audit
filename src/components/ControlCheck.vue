8e53988496e3365be27c82e28a1305988c2da663
Carlosc23 <cal15219@uvg.edu.gt>
2019-09-21 21:27:30 -0600

    Update ControlCheck.vue

---
+++ src/components/ControlCheck.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Cumplimiento de control</h2>
+    <b-table-simple hover small caption-top responsive>
+      <colgroup>
+        <col />
+        <col />
+      </colgroup>
+      <colgroup>
+        <col />
+        <col />
+        <col />
+      </colgroup>
+      <colgroup>
+        <col />
+        <col />
+      </colgroup>
+      <b-thead head-variant="dark">
+        <b-tr>
+          <b-th>Objetivos</b-th>
+          <b-th>Estado</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr>
+          <b-th>Políticas de Seguridad de Información</b-th>
+          <b-th variant="danger"></b-th>
+        </b-tr>
+      </b-tbody>
+   
+    </b-table-simple>
+  </div>
+</template>
